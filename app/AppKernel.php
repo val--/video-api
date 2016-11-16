1697febbdefbd8d1bc80ca0c6779fd73f4cc9395
val-- <khayvav@gmail.com>
2016-11-16 08:50:31 +0100

    Added console use through browser (CoreSphereConsoleBundle)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
+            new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/VideoBundle/Command/AddVideoCommand.php
+++ src/VideoBundle/Command/AddVideoCommand.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class AddVideoCommand extends ContainerAwareCommand
@@ -30,16 +31,67 @@ class AddVideoCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln([
-        'Video Creator',
-        '============',
+/*        $output->writeln([
+        '=============================',
+        '[* *]   Video Creator   [* *]',
+        '=============================',
         '',
-	    ]);
-        $videoFactory = $this->getContainer()->get('video.factory');
-        $videoFactory->createVideo($input->getArgument('title'),new \DateTime($input->getArgument('date')),$input->getArgument('realisator'));
+	    ]);*/
 
-        $output->writeln('Video successfully created!');
-	    // retrieve the argument value using getArgument()
-	    //$output->writeln('Video title: '.$input->getArgument('title'));
+        $output->writeln([
+        '.------------------------.',         
+        '|\\//////  Video Creator  |',      
+        '| \/  __  ______  __     |',             
+        '|    /  \|\.....|/  \    |',             
+        '|    \__/|/_____|\__/    |',             
+        '| A                      |',             
+        '|    ________________    |',             
+        '|___/_._o________o_._\___|',
+        '',
+        ]);
+/*
+        $date = "2016-10-10 12:10:45";
+        $title = "Video";
+        $realisator = "Director";
+*/
+
+        $videoFactory = $this->getContainer()->get('video.factory');
+/*        $helper = $this->getHelper('question');*/
+
+/*        $question_title = new Question('Please enter the name of the video : ', 'Video');
+        $question_title->setValidator(function ($answer) {
+        if (strlen($answer) > 39){
+            throw new \RuntimeException('The name of the video must be less than 40 characters.');
+        }
+        else if(strlen($answer) < 3){
+            throw new \RuntimeException('The name of the video must be more than 2 characters.');
+        }
+        return $answer;
+        });
+        $question_title->setMaxAttempts(20);
+        $title = $helper->ask($input, $output, $question_title);
+
+
+        $question_date = new Question('Please enter the date (datetime format, ex. "2016-10-10 12:10:45") of the video : ', '2016-10-10 12:10:45');
+        $date = $helper->ask($input, $output, $question_date);
+
+
+        $question_realisator = new Question('Please enter the director of the video : ', 'Director');
+        $realisator  = $helper->ask($input, $output, $question_realisator);
+*/  
+
+        $title = $input->getArgument('title');
+        $date = $input->getArgument('date');
+        $realisator = $input->getArgument('realisator');
+
+        $pattern = "(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})";
+        if(preg_match($pattern, $date)){
+            $date = new \DateTime($input->getArgument('date'));
+        }else{
+            throw new \RuntimeException('Wrong date format for this video (datetime expected)');   
+        }
+
+        $videoFactory->createVideo($title, $date, $realisator);
+        $output->writeln('Video "'.$title.'" successfully created!');
 	}
 }
